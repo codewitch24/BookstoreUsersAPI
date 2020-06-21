d6798bb26dcab65abf35de180e563f499bfcefdb
mostafa <mostafa.babaii@gmail.com>
2020-06-22 01:19:35 +0430

    Add user model DTO and DAO.

---
+++ domain/users/UserDAO.go
@@ -0,0 +1,13 @@
+package users
+
+import (
+	"github.com/codewitch24/BookstoreUsersAPI/utils/errors"
+)
+
+func (user User) Get() *errors.RestError {
+	return nil
+}
+
+func (user User) Save() *errors.RestError {
+	return nil
+}
