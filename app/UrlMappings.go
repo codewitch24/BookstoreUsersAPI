25b8165f90dcf7c83f1d53db2164fbb7eab22563
mostafa <mostafa.babaii@gmail.com>
2020-06-21 11:02:22 +0430

    Add some routes.

--- app/UrlMappings.go
+++ app/UrlMappings.go
@@ -1,11 +1,14 @@
 package app
 
 import (
-	"github.com/codewitch24/BookstoreUsersAPI/controllers"
+	PingController "github.com/codewitch24/BookstoreUsersAPI/controllers/ping"
+	UsersController "github.com/codewitch24/BookstoreUsersAPI/controllers/users"
 	"github.com/gin-gonic/gin"
 )
 
 func MapUrls(r *gin.Engine) *gin.Engine {
-	r.GET("/ping", controllers.Ping)
+	r.GET("/ping", PingController.Ping)
+	r.POST("/users", UsersController.CreateUser)
+	r.GET("/users/:UserId", UsersController.GetUser)
 	return r
 }
